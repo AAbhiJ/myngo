41a08cb4c790cfce57d55b9adf2b09937264dc95
AAbhiJ <abhiasawale510@gmail>
2023-11-16 19:42:34 +0530

    extracted base url to env file

--- utils/fetcher.js
+++ utils/fetcher.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 import Cookies from 'js-cookie';
 
+const baseURL = process.env.BASE_URL
+if(baseURL==="")
+baseURL = "http://localhost:3000"
+
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: `${baseURL}/api`,
 });
 
 // axiosInstance.interceptors.request.use(
